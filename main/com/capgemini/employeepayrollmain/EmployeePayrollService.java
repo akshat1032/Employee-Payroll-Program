ef2f57ad161387dfd42c8ed818100e9462802c1c
akshat1032 <akshat.30oct@gmail.com>
2020-10-15 22:09:36 +0530

    Added : Welcome message added

---
+++ main/com/capgemini/employeepayrollmain/EmployeePayrollService.java
@@ -0,0 +1,12 @@
+package main.com.capgemini.employeepayrollmain;
+
+import java.util.logging.*;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		
+		Logger log = Logger.getLogger(EmployeePayrollService.class.getName());
+		//Welcome message added
+		log.info("Welcome to Employee Payroll Program");
+	}
+}
